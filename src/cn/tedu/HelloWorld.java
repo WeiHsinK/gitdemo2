6e5e7a75173b182886466376f57f8de17c9e4274
a121212106 <a121212106@gmail.com>
2023-07-31 03:33:06 +0800

    sqy bye

--- src/cn/tedu/HelloWorld.java
+++ src/cn/tedu/HelloWorld.java
@@ -3,5 +3,6 @@ package cn.tedu;
 public class HelloWorld {
     public static void main(String[] args) {
         System.out.printf("hello world");
+        System.out.printf("goodbye world");
     }
 }
